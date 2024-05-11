d769d2e2d2c89ac1fc39e8e7a11a0f13fdde0e38
Jose Luis Cerrato Rosales <luiscerrato9@gmail.com>
2024-05-10 20:47:35 -0600

    test

---
+++ src/Saludar.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Luis Cerrato
+ */
+public class Saludar {
+    
+    public static void main(String args[]){
+
+    
+        System.out.println("saludar");
+
+}
+
+    
+}
